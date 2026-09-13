c3e79ce6f2bb910d6c9577a1e569a8482b6f7e54
rhdCode0x7C3 <rhdCode0x7C3@icloud.com>
2026-09-13 09:06:18 +0100

    rhkv finished

--- lib/rhkv.sh
+++ lib/rhkv.sh
@@ -3,115 +3,150 @@
 # Persistent key value store
 # Written by rh 2026-09-08
 #
-# Environment variables:
-# RHKV_FILE
-# /path/to/file
-#
 # Usage:
 # Keys must be valid bash variable names
+# Values are arbitrary single line strings
+# API:
+# rhkv_get KEY FILE
+# rhkv_del KEY FILE
+# rhkv_put KEY VALUE FILE (overwrites existing values)
+# rhkv_lst FILE
 
 # Regex patterns
-_rhkv_varp='[_[:alpha:]][_[:alpha:][:digit:]]*'
-_rhkv_kvp="^${_rhkv_varp}=.*$"
+_rhkv_keyp='[_[:alpha:]][_[:alpha:][:digit:]]*'
 
 # Errors
 _rhkv_err_nf() {
-  s="$1"
-  printf "NOT FOUND: %s\n" "$s"
+  local s="$1"
+  printf "NOT FOUND: %s\n" "$s" >&2
   return 1
 }
 
 _rhkv_err_iv() {
-  s="$1"
-  printf "INVALID: %s\n" "$s"
+  local s="$1"
+  printf "INVALID: %s\n" "$s" >&2
   return 1
 }
 
-_rhkv_process() {
-  # Usage: _rhkv_process f file
-  local f="$1"
-  shift
-  while IFS= read -r line; do
-    # [[ $line =~ ^[A-Za-z_]+[A-Za-z0-9_]+=[A-Za-z0-9_[:blank:]]+$ ]] || continue
-    [[ $line =~ ${_rhkv_kvp} ]] || continue
-    "$f" "$line"
-  done <"$1"
+# Private utility functions
+_rhkv_is_key() {
+  local s="$1"
+  [[ "$s" =~ ^${_rhkv_keyp}$ ]]
 }
 
 _rhkv_is_kv() {
   local s="$1"
-  [[ $s =~ ${_rhkv_kvp} ]]
-}
-
-_rhkv_key_of_kv() {
-  # Takes a key=value string and returns the key
-  local s="$1"
-  if _rhkv_is_kv "$s"; then
-    echo "${s%%=*}"
-    return 0
-  else
-    _rhkv_err_iv "$s"
-  fi
+  [[ "$s" =~ ^${_rhkv_keyp}=.*$ ]]
 }
 
 _rhkv_val_of_kv() {
   # Takes a key=value string and returns the value
   local s="$1"
   if _rhkv_is_kv "$s"; then
-    echo "${s#*=}"
+    printf '%s\n' "${s#*=}"
     return 0
   else
     _rhkv_err_iv "$s"
   fi
 }
 
-_rhkv_export() {
-  # Takes a key=value string and exports to an enrionment variable
-  local d='='
-  local s="$1"
-  local key="${s%%${d}*}"
-  local val="${s#*${d}}"
-  export "$key"="$val"
-}
-
 # Public API
 rhkv_get() {
   # Usage: rhkv_get "ESP_PARTID" myfile.txt
   # Takes a key and returns the associated value
-  local key="$1"
-  local file="$2"
-  local line
-  if line="$(grep "$key" "$file")"; then
+  local key="$1" file="$2" line
+
+  _rhkv_is_key "$key" || {
+    _rhkv_err_iv "$key"
+    return 1
+  }
+
+  if line="$(grep -m 1 "^${key}=" "$file")"; then
     _rhkv_val_of_kv "$line"
   else
     _rhkv_err_nf "$key"
   fi
 }
-# rhkv_del() {}
-# rhkv_put() {}
+rhkv_del() {
+  # Usage: rhkv_del "ROOT_PARTID" myfile.txt
+  # Takes a key and deletes the key=value pair from the file
+  local key="$1" file="$2" line_n
+
+  _rhkv_is_key "$key" || {
+    _rhkv_err_iv "$key"
+    return 1
+  }
+
+  if line_n="$(grep -nm 1 "^${key}=" "$file")"; then
+    line_n="${line_n%%:*}"
+    sed -i "${line_n}d" "$file"
+    return 0
+  fi
+  _rhkv_err_nf "$key"
+  return 1
+}
+
+rhkv_put() {
+  # Usage: rhkv_put HOSTNAME "my server" myfile.txt
+  # Note: existing keys will be overwritten
+  local key="$1" val="$2" file="$3"
+  # found=0: key has not been read
+  # found=1: key has been read
+  local tmp found=0 line
+
+  # Check the key is valid
+  _rhkv_is_key "$key" || {
+    _rhkv_err_iv "$key"
+    return 1
+  }
+
+  # Check the file exists
+  # If not, create it
+  [[ -f "$file" ]] || touch "$file"
+
+  # Create a temp file
+  # Return 1 if this fails
+  tmp=$(mktemp "${file}.XXXXXX") || return 1
+
+  # Read lines from stdin
+  while IFS= read -r line || [[ -n $line ]]; do
+    # If the key in this line matches the supplied key
+    if [[ $line == "$key="* ]]; then
+      # And if the key has not been read
+      if ((!found)); then
+        # Print "key=val" to stdout
+        printf '%s=%s\n' "$key" "$val"
+        found=1
+      fi
+    else
+      # Copy the line to stdout
+      printf '%s\n' "$line"
+    fi
+    # $file goes to this block's stdin
+    # stdout goes to $tmp
+  done <"$file" >"$tmp"
+
+  # If the key has not been read
+  if ((!found)); then
+    # Append "key=val" to $tmp
+    printf '%s=%s\n' "$key" "$val" >>"$tmp"
+  fi
+
+  mv -- "$tmp" "$file" || {
+    rm -f -- "$tmp"
+    return 1
+  }
+}
 
 rhkv_lst() {
   # Usage: rhkv_lst myfile.txt
   # Prints all key value pairs
 
-  local file
-  file=$1
+  local file="$1"
 
-  local f="echo"
-
-  _rhkv_process "$f" "$file"
-
-}
-
-rhkv_read() {
-  # Usage: rhkv_read myfile.txt
-  # Exports all key value pairs to the environment
-
-  local file
-  file=$1
-
-  local f="_rhkv_export"
-
-  _rhkv_process "$f" "$file"
+  while IFS= read -r line || [[ -n $line ]]; do
+    _rhkv_is_kv "$line" || continue
+    printf "%s\n" "$line"
+  done <"$file"
 
 }
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,12 @@
 # 000_setup.sh
 # Written by rh 2026-09-08
 
+export SCRIPT_DIR
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+
+source "$SCRIPT_DIR/lib/rhl.sh"
+source "$SCRIPT_DIR/lib/rhkv.sh"
+
 run_id_gen() {
   local timestamp
   timestamp="$(date +%s%N)"
@@ -13,3 +19,8 @@ run_id_gen() {
 }
 
 export RUN_ID=run_id_gen
+export FILES_DIR=/tmp/rh_linux_install && mkdir -p $FILES_DIR
+
+export RHL_LOGFILE=$FILES_DIR/${RUN_ID}/log && touch $RHL_LOGFILE
+export RHKV_MANIFEST=$FILES_DIR/${RUN_ID}/manifest && touch $RHKV_MANIFEST
+export RHKV_CHECKPOINTS=$FILES_DIR/${RUN_ID}/checkpoints && touch $RHKV_CHECKPOINTS
